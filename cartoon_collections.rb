5507ea5d0dd362e4a729f374ef9b001482461b01
Mike Dilley <mike.dilley@gmail.com>
2018-10-11 15:28:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.collect {|call| array.capitalize + "!"}
+  array.collect {|call| call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
