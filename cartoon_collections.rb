762e01ab6f8d3f09459a77e8dbc58264fce3b9c3
Mike Dilley <mike.dilley@gmail.com>
2018-10-11 15:30:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(array)# code an argument here
   array.collect {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.collect {|call| call.length > 4}.all?
 end
 
 def find_the_cheese# code an argument here
